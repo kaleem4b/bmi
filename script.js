af4c69d0bc1065babbff20111e166faf8a276df5
kaleem4b <79290497+kaleem4b@users.noreply.github.com>
2021-02-20 16:51:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function bmi() {
+    var height = parseFloat(document.getElementById("height").value)
+    var weight = parseFloat(document.getElementById("weight").value)
+    var bmi = weight/(height*height);
+    var userBMI = document.getElementById("bmi_index")
+    userBMI.textContent = bmi.toFixed(2);
+}
+
+function cm() {
+    var cm = parseFloat(document.getElementById("cm").value)
+    var cm = cm/100;
+    var userM = document.getElementById("cm-to-m")
+    userM.textContent = cm.toFixed(2);
+}
